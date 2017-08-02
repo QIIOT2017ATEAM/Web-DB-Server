4bd4464c1ef23faeb5db99a11640266300e18eb6
bho7982 <bho7982@naver.com>
2017-08-02 14:27:38 -0700

    Revert "Revert "Merge branch 'Bae'""
    This reverts commit 20f632e089d09d1a6eedf330eaa3f0e973c692e7.

--- finally/slim-api/routes/maps.php
+++ finally/slim-api/routes/maps.php
@@ -1,11 +1,6 @@
 <?php 
 <<<<<<< HEAD
-<<<<<<< HEAD
 //PHPMailer include slim start
-=======
-
-//PHPMailer include slim
->>>>>>> parent of d2d97f4... Merge branch 'Bae'
 $app->post('/send-email', function () use ($app) {
     include "db_functions.php";
     include '../signup_confirmation/connection/connect.php';
@@ -95,14 +90,12 @@ if(isset($_POST['sign_up_btn']))
 	}
 }	
 });
+//PHPMailer include slim end
 
-<<<<<<< HEAD
 //heroes-as-json start
 =======
 
 >>>>>>> parent of f779fa3... Merge branch 'Bae'
-=======
->>>>>>> parent of d2d97f4... Merge branch 'Bae'
 $app->get('/heroes-as-json', function () use ($app) {
     include "db_functions.php";
 
@@ -124,7 +117,7 @@ $app->get('/heroes-as-json', function () use ($app) {
                 //$person_array[] = array($person['codename'] => array("center" => array("lat" => $person['lat'], "lng" => $person['lng']), "population" => "20000000"));
                 $person_array[$person['codename']] = array("center" => array(  "lat" => $person['lat'], 
                                                                                 "lng" => $person['lng']),
-                                                            "population" => "10");
+                                                                                "population" => "10");
             }
             $app->response->headers->set('Content-Type', 'application/json');
             $app->response->setStatus(200);
@@ -137,12 +130,14 @@ $app->get('/heroes-as-json', function () use ($app) {
         return $app->response;
     }
 });
+//heroes-as-json end
 
+//air-as-json start
 $app->get('/air-as-json', function () use ($app) {
     include "db_functions.php";
 
     try {
-        $sth = $pdo->prepare('SELECT * FROM airdata');
+        $sth = $pdo->prepare('SELECT * FROM Air_Data');
         $sth->execute();
 
         $result = $sth->fetchAll();
@@ -157,9 +152,9 @@ $app->get('/air-as-json', function () use ($app) {
             $person_array = [];
             foreach ($result as $person) {
                 //$person_array[] = array($person['codename'] => array("center" => array("lat" => $person['lat'], "lng" => $person['lng']), "population" => "20000000"));
-                $person_array[$person['Air_ID']] = array("center" => array( "population" => $person['data'], 
-                                                                            "lat" => $person['lat'], 
-                                                                             "lng" => $person['lng']));
+                $person_array[$person['Air_ID']] = array("center" => array( "lat" => $person['lat'], 
+                                                                            "lng" => $person['lng']),
+                                                                            "Air_One" => $person['Air_One']);
             }
             $app->response->headers->set('Content-Type', 'application/json');
             $app->response->setStatus(200);
@@ -172,6 +167,7 @@ $app->get('/air-as-json', function () use ($app) {
         return $app->response;
     }
 });
+//air-as-json start
 
 $app->get('/my-map', function () use ($app) {
     // if you want to pass variables to the template, put them in $data.
